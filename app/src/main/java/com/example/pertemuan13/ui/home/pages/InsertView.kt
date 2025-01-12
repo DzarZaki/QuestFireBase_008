9ef0f547a22afba64edfc5566c57fd01c6d667e5
DzarZaki <dzarzaki2@gmail.com>
2025-01-12 11:34:03 +0700

    Menambahkan InsertBodyMhs dengan form input dan tombol submit dengan indikator loading

--- app/src/main/java/com/example/pertemuan13/ui/home/pages/InsertView.kt
+++ app/src/main/java/com/example/pertemuan13/ui/home/pages/InsertView.kt
@@ -1,7 +1,9 @@
 package com.example.pertemuan13.ui.home.pages
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -11,10 +13,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertemuan13.ui.PenyediaViewModel
 import com.example.pertemuan13.ui.home.viewmodel.FormState
+import com.example.pertemuan13.ui.home.viewmodel.InsertUiState
 import com.example.pertemuan13.ui.home.viewmodel.InsertViewModel
+import com.example.pertemuan13.ui.home.viewmodel.MahasiswaEvent
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.lang.reflect.Modifier
@@ -85,6 +91,43 @@ fun InsertMhsView(
                     }
                 }
             )
+        }
+    }
+}
+@Composable
+fun InsertBodyMhs(
+    modifier: Modifier = Modifier,
+    onValueChange: (MahasiswaEvent) -> Unit,
+    uiState: InsertUiState,
+    onClick: () -> Unit,
+    homeUiState: FormState
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        FormMahasiswa(
+            mahasiswaEvent = uiState.insertUiEvent,
+            onValueChange = onValueChange,
+            errorState = uiState.isEntryValid,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onClick,
+            modifier = Modifier.fillMaxWidth(),
+            enabled = homeUiState !is FormState.Loading,
+        ) {
+            if (homeUiState is FormState.Loading) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .size(20.dp)
+                        .padding(end = 8.dp)
+                )
+                Text("Loading...")
+            } else {
+                Text("Add")
+            }
         }
     }
 }
